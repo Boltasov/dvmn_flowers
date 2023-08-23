f11bff864ceb85690103be47affc1580aae3f020
Kilsik <ilsiwow@gmail.com>
2023-08-23 19:13:41 +0300

    Begin

--- shop/views.py
+++ shop/views.py
@@ -8,9 +8,9 @@ from dvmn_flowers.settings import MEDIA_ROOT, MEDIA_URL, STATIC_ROOT
 def main_page(request):
     popular_bouquets = Bouquet.objects.all()\
         .annotate(count=Count('orders')).order_by('-count')[:3]
-    print(MEDIA_ROOT)
-    print(MEDIA_URL)
-    print(STATIC_ROOT)
     return render(request, 'index.html', {
         'recomendations': popular_bouquets,
         })
+
+def catalog(request):
+    catalog_bouquets = Bouquet.objects.all()
